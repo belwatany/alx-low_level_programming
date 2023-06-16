76e67bd91a360f80a39f8ac70d0ba1d78319b54f
Basel_ElWatany <b.elwatany@gmail.com>
2023-06-16 11:29:30 -0700

    All_Done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,23 +10,13 @@
 
 int main(void)
 {
-	char ch = 'a';
-	char ch = 'A';
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-	/*prints a - z*/
-	while (ch <= 'z')
+	for (i = 0; i < 52; i++)
 	{
-		putchar(ch);
-		ch++;
-	}
-
-	/*prints A - Z*/
-	while (ch <= 'Z')
-	{
-		putchar(CH);
-		CH++;
+		putchar(alp[i]);
 	}
 	putchar('\n');
-
 	return (0);
 }
