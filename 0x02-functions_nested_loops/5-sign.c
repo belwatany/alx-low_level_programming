363b9a005639ea5228f438d5dae58ddbe3af1a7b
Basel_ElWatany <b.elwatany@gmail.com>
2023-06-20 12:01:26 -0700

    All_Done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,22 @@
  * -1 and prints - if n is less than zero
  */
 
-int print_sign(int n);
+int print_sign(int n)
+
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
 	}
 }
