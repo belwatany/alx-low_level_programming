2d84e0f3767d3619b4acf44677e80c92d5c4edb2
Basel_ElWatany <b.elwatany@gmail.com>
2023-06-21 04:49:41 -0700

    Done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,6 @@
 * main - Test function for positive or negative
 * Return: 0
 */
-
 int main(void)
 {
 	int n;
@@ -11,10 +10,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive\n", n);
+	{
+		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
-		printf("%i is zero\n", n);
+	{
+		printf("%d is zero\n", n);
+	}
 	else
-		printf("%i is negative\n", n);
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
