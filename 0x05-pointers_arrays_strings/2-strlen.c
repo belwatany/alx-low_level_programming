2baac7d2a22f8802d8f878d2569b99767541b863
Basel_ElWatany <b.elwatany@gmail.com>
2023-06-26 10:04:14 -0700

    Done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,11 +9,13 @@
 
 int _strlen(char *s)
 {
-	int len = 0;
-    while (*s != '\0')
-    {
-	    len++;
-	    s++;
-    }
-    return (len);
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
 }
